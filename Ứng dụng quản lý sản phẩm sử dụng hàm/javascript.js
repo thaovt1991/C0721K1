fcc36176c51f6cc9e43c6d0ef6ed594400f2106d
thaovt91 <vothao.tin@gmail.com>
2021-08-18 13:39:41 +0700

    up

--- Ứng dụng quản lý sản phẩm sử dụng hàm/javascript.js
+++ Ứng dụng quản lý sản phẩm sử dụng hàm/javascript.js
@@ -1,31 +1,67 @@
-let arrProduct = new Array()
-let x = 0 ;
-function addProduct(){
-    arrProduct[x] = document.getElementById("inputProduct").value ;
-    document.getElementById("inputProduct").value = "";
-    document.getElementById("inputProduct").focus()
-    document.getElementById("P"+x).innerHTML = arrProduct[ parseInt(x)] ;
-    document.getElementById("editP"+x).style.display = "block"
-    document.getElementById("deleteP"+x).style.display = "block"
-    x++ ;
+let arrProduct = new Array()   //("Nokia", "Sony", "SamSung")
+function displayProduct() {
+    document.getElementById("totalProduct").innerHTML = arrProduct.length + " Product "
+    for (i = 0; i < arrProduct.length; i++) {
+        document.getElementById("P" + i).innerHTML = arrProduct[i];
+        document.getElementById("editP" + i).style.display = "block"
+        document.getElementById("deleteP" + i).style.display = "block"
+    }
 
 }
 
 
+function addProduct() {
+    let value = document.getElementById("inputProduct").value;
+    if (value != "") {
+        if (arrProduct.indexOf(value) == -1) {
+            arrProduct.push(value)
+            let i = arrProduct.length - 1
+            document.getElementById("inputProduct").value = "";
+            document.getElementById("inputProduct").focus()
+            document.getElementById("editP" + i).style.display = "block"
+            document.getElementById("deleteP" + i).style.display = "block"
+        } else {
+            alert("Sản phẩm đã có trong danh sách.Hãy nhập lại")
+            document.getElementById("inputProduct").focus()
+        }
+    }
+    else {
+        alert("Sản phẩm không được để trống")
+        document.getElementById("inputProduct").focus()
+    }
+    displayProduct()
+}
+function deleteDisplayLast() {
+    let last = arrProduct.length;
+    document.getElementById("edit" + last).style.display = "none";
+    document.getElementById("deleteP" + last).style.display = "none";
+    document.getElementById(last).innerHTML = ""
+}
 
+function DeleteProduct(clicker_id) {
+    let num = parseInt((clicker_id).replace("deleteP", ""))
+    let str = (clicker_id).replace("delete", "");
+    arrProduct.splice(num, 1)
+    document.getElementById("edit" + str).style.display = "none";
+    document.getElementById(clicker_id).style.display = "none";
+    document.getElementById(str).innerHTML = "";
+    displayProduct()
+}
+function EditProduct(clicker_id) {
+    let num = (clicker_id).replace("editP", "")
+    let str = (clicker_id).replace("edit", "");
+    document.getElementById("renamePro" + num).style.display = "block";
+}
+function RenameProduct(clicker_id) {
+    let num = (clicker_id).replace("done", "")
+    if (document.getElementById("rename" + num).value != '') {
+        arrProduct[num] = document.getElementById("rename" + num).value
+        document.getElementById("renamePro" + num).style.display = "none"
+        displayProduct()
+    } else {
+        alert("Không để trống tên");
+        document.getElementById("rename" + num).focus()
+    }
+    document.getElementById("rename" + num).value ="" ;
 
-
-
-
-
-
-
-function DeleteProduct(clicker_id){
-    let num = (clicker_id).replace("deleteProduct","")
-    let str = (clicker_id).replace("delete","") ;
-    document.getElementById("edit"+str).style.display = "none";
-    document.getElementById(clicker_id).style.display = "none" ;
-    document.getElementById(str).innerHTML = ""
-    arrProduct.splice(num,1)
-    
 }
